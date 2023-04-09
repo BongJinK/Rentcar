5f558227d3cd157d6cf380574d465c3dc3835d78
Bong Jin <qhdwls013@naver.com>
2023-04-09 10:52:08 +0900

    delete anotation

--- RentCar/src/main/java/board/controller/BoardDao.java
+++ RentCar/src/main/java/board/controller/BoardDao.java
@@ -201,40 +201,13 @@ public class BoardDao {
 	}
 
 	// U
-	// 수정하기
-	public void updateboard(BoardRequestDto boardDto) {
-		this.conn = DBManager.getConnection();
-
-		if (this.conn != null) {
-			String sql = "UPDATE board SET title = ?, content= ? WHERE board_number = ?";
-//			String sql = "UPDATE board SET title = ?, content= ?";
-//			sql += "WHERE board_number = ?";
-
-			try {
-				this.pstmt = this.conn.prepareStatement(sql);
-				this.pstmt.setString(1, boardDto.getTitle());
-				System.out.println(boardDto.getTitle());
-				this.pstmt.setString(2, boardDto.getContent());
-				System.out.println(boardDto.getContent());
-				this.pstmt.setInt(3, boardDto.getBoardNum());
-				
-				this.pstmt.executeUpdate();
-			
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, pstmt);
-			}
-		}
-	}
-	
+	// 수정하기	
 	public void updateBoard(BoardRequestDto boardDto) {
 		this.conn = DBManager.getConnection();
 		
 		if(this.conn != null) {
-			String sql = "UPDATE test.board SET title=?,content=? WHERE board_number=?";
+			String sql = "UPDATE board SET title=?,content=? WHERE board_number=?";
 			
-			System.out.println("sql ==> " + sql);
 			try {
 				pstmt = this.conn.prepareStatement(sql);
 				pstmt.setString(1, boardDto.getTitle());
