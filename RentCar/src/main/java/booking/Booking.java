6faf9a26708204a4d053a5f45f117f32fc101b22
Bong Jin <qhdwls013@naver.com>
2023-04-09 19:38:34 +0900

    update booking vo, dto done

--- RentCar/src/main/java/booking/Booking.java
+++ RentCar/src/main/java/booking/Booking.java
@@ -1,5 +1,70 @@
 package booking;
 
+import java.sql.Timestamp;
+
 public class Booking {
+
+	private String bookingCode;
+	private Timestamp regDate;
+	private int rentalTime;
+	private Timestamp bookingDate;
+	private Timestamp returnDate;
+
+	private String clientId;
+	private String driverCode;
+	private String vehicleCode;
+
+	public Booking(String bookingCode, Timestamp regDate, int rentalTime, String clientId, Timestamp bookingDate,
+			Timestamp returnDate, String driverCode, String vehicleCode) {
+		this.bookingCode = bookingCode;
+		this.regDate = regDate;
+		this.rentalTime = rentalTime;
+		this.bookingDate = bookingDate;
+		this.returnDate = returnDate;
+		this.driverCode = driverCode;
+		this.vehicleCode = vehicleCode;
+	}
+	
+	public Booking(BookingRequestDto bookingDto) {
+		this.bookingCode = bookingDto.getBookingCode();
+		this.regDate = bookingDto.getRegDate();
+		this.rentalTime = bookingDto.getRentalTime();
+		this.bookingDate = bookingDto.getBookingDate();
+		this.returnDate = bookingDto.getReturnDate();
+		this.driverCode = bookingDto.getDriverCode();
+		this.vehicleCode = bookingDto.getVehicleCode();
+	}
+
+	public String getBookingCode() {
+		return bookingCode;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public int getRentalTime() {
+		return rentalTime;
+	}
+	
+	public String getClientId() {
+		return clientId;
+	}
+
+	public Timestamp getBookingDate() {
+		return bookingDate;
+	}
+
+	public Timestamp getReturnDate() {
+		return returnDate;
+	}
+
+	public String getDriverCode() {
+		return driverCode;
+	}
+
+	public String getVehicleCode() {
+		return vehicleCode;
+	}
 
 }
--- RentCar/src/main/java/booking/BookingRequestDto.java
+++ RentCar/src/main/java/booking/BookingRequestDto.java
@@ -1,5 +1,80 @@
 package booking;
 
+import java.sql.Timestamp;
+
 public class BookingRequestDto {
+
+	private String bookingCode;
+	private Timestamp regDate;
+	private int rentalTime;
+	private Timestamp bookingDate;
+	private Timestamp returnDate;
+
+	private String clientId;
+	private String driverCode;
+	private String vehicleCode;
+
+	public BookingRequestDto(String bookingCode, Timestamp regDate, int rentalTime, String clientId,
+			Timestamp bookingDate, Timestamp returnDate, String driverCode, String vehicleCode) {
+		this.bookingCode = bookingCode;
+		this.regDate = regDate;
+		this.rentalTime = rentalTime;
+		this.bookingDate = bookingDate;
+		this.returnDate = returnDate;
+		this.driverCode = driverCode;
+		this.vehicleCode = vehicleCode;
+	}
+
+	public String getBookingCode() {
+		return bookingCode;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public int getRentalTime() {
+		return rentalTime;
+	}
+
+	public void setRentalTime(int rentalTime) {
+		this.rentalTime = rentalTime;
+	}
+	
+	public String getClientId() {
+		return clientId;
+	}
+
+	public Timestamp getBookingDate() {
+		return bookingDate;
+	}
+
+	public void setCreatedDate(Timestamp bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+
+	public Timestamp getReturnDate() {
+		return returnDate;
+	}
+
+	public void setModifiedDate(Timestamp returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public String getDriverCode() {
+		return driverCode;
+	}
+
+	public void setDriverCode(String driverCode) {
+		this.driverCode = driverCode;
+	}
+
+	public String getVehicleCode() {
+		return vehicleCode;
+	}
+
+	public void setVehicleCode(String vehicleCode) {
+		this.vehicleCode = vehicleCode;
+	}
 
 }
